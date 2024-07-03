b6e8d71c2db01cfb83913ebf2efe8c58e851a69e
Zimil-Patel <patelzimil2310@gmail.com>
2024-07-03 15:26:41 +0530

    03July24

---
+++ practicals/arithmetic.dart
@@ -0,0 +1,28 @@
+void main() {
+  final int a = 10;
+  int b = 5;
+
+  // Addition
+  int addition = a + b;
+  print("Addition: $a + $b = $addition");
+
+  // Subtraction
+  int subtraction = a - b;
+  print("Subtraction: $a - $b = $subtraction");
+
+  // Multiplication
+  int multiplication = a * b;
+  print("Multiplication: $a * $b = $multiplication");
+
+  // Division
+  double division = a / b;
+  print("Division: $a / $b = ${division.toString().padRight(5, 'x')}");
+
+  // Integer Division
+  int intDivision = a ~/ 3;
+  print("Integer Division: $a ~/ $b = $intDivision");
+
+  // Modulus
+  int modulus = a % 3;
+  print("Modulus: $a % $b = $modulus");
+}
---
+++ practicals/cascade.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+class CascadeOpe {
+  late int _a, _b, _c;
+  static final int a = 10;
+  aValue(int a) {
+    this._a = a;
+  }
+
+  bValue(int? b) {
+    b ??= 20;
+    this._b = b;
+  }
+
+  cValue(int? c) {
+    this._c = c ?? 30;
+  }
+
+  printAll() {
+    print("a : $_a");
+    print("b : $_b");
+    print("c : $_c");
+    print("sum of all : ${_a + _b + _c}");
+  }
+}
+
+void main() {
+  CascadeOpe()
+    .._a = 10
+    ..aValue(int.parse(stdin.readLineSync()!))
+    ..bValue(null)
+    ..cValue(null)
+    ..printAll();
+
+  // print([1, 2, 3, 4] is List);
+
+  int z = 10 > 5 ? 10 : 5;
+
+  List myList = [1, 2, 3, 4]
+    ..add(5)
+    ..sort()
+    ..clear();
+
+  var x = 10;
+  print(x);
+  x = 15;
+  print(x);
+}
